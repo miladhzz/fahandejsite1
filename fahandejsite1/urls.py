6c48545bd1a8d05011a8976b5808d8d55813710f
Milad Hatami <miladhzz@gmail.com>
2020-01-02 19:41:42 +0000

    add language

--- fahandejsite1/urls.py
+++ fahandejsite1/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
+from website import views
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('loginmodir/', admin.site.urls),
-    path('', include('website.urls'))
-]
+    path('', views.index, name='index'),
+    prefix_default_language=False
+)
+
