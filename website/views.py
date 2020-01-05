a04ccbcbe434db57c24d97a98161e16d4703566c
Milad Hatami <miladhzz@gmail.com>
2020-01-05 11:05:54 +0330

    add footer gallery

--- website/views.py
+++ website/views.py
@@ -1,11 +1,16 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 from . import models
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def index(request):
     sliders = models.Slider.objects.all()[:3]
-    gallery_footer = models.Gallery.objects.get(category__name='Gallery Footer').pictures
+    # gallery_footer = None
+    try:
+        gallery_footer = models.Gallery.objects.get(category__name='Gallery Footer').pictures.all()
+    except ObjectDoesNotExist:
+        gallery_footer = None
     print(gallery_footer)
     return render(request, "index.html", {'sliders': sliders,
                                           'gallery_footer': gallery_footer})
