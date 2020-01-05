3495179933bfab94c4a5800e1e312b58c313ae11
Milad Hatami <miladhzz@gmail.com>
2020-01-05 15:07:14 +0330

    fix name

--- website/views.py
+++ website/views.py
@@ -6,14 +6,19 @@ from django.core.exceptions import ObjectDoesNotExist
 
 def index(request):
     sliders = models.Slider.objects.all()[:3]
-    # gallery_footer = None
+    # gallery_footer
     try:
         gallery_footer = models.Gallery.objects.get(category__name='Gallery Footer').pictures.all()
     except ObjectDoesNotExist:
         gallery_footer = None
     
-    product_3_top_3 = models.Product.objects.filter(category__name='3 Top Product')[:3]
+    # product 3 top 
+    product_3_top = models.Product.objects.filter(category__name='3 Top Product')[:3]
+
+    # Banner 2 top
+
+
     print(product_3_top_3)
     return render(request, "index.html", {'sliders': sliders,
                                           'gallery_footer': gallery_footer,
-                                          'product_3_top_3': product_3_top_3})
+                                          'product_3_top': product_3_top})
