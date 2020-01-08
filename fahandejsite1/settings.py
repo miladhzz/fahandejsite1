45ce1d73546745c7705d033e929df017f98f887a
Milad Hatami <miladhzz@gmail.com>
2020-01-08 22:12:46 +0000

    change home url

--- fahandejsite1/settings.py
+++ fahandejsite1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 secret = json.load(open(os.path.join(BASE_DIR, 'secret.json')))
 secret_dev = json.load(open(os.path.join(BASE_DIR, 'secret_dev.json')))
