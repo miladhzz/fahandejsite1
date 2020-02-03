193d32b89c1005fe08d93fd17c34098b74925452
Milad Hatami <miladhzz@gmail.com>
2020-02-03 12:21:22 +0330

    update media root url

--- fahandejsite1/settings.py
+++ fahandejsite1/settings.py
@@ -156,5 +156,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/home/fahandej/public_html/media/'
+MEDIA_ROOT = '/home/fahandej/public_html/media/'
+MEDIA_URL = '/media/'
